427b4afa84fb94a7ed4fc5fe56a6783323e94b1f
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-07 18:30:02 +0300

    made save manager save the gamestate as a whole

--- Assets/Code/Services/DataManagement/SaveManager.cs
+++ Assets/Code/Services/DataManagement/SaveManager.cs
@@ -1,3 +1,4 @@
+using Code.DataClasses;
 using Code.Services.DataManagement.Serializers;
 using Code.Services.DataManagement.Storage;
 using System.Threading.Tasks;
@@ -14,14 +15,14 @@ namespace Code.Services.DataManagement {
             _serializer = serialization;
         }
 
-        public async Task SaveAsync<T>(string key, T data) {
+        public async Task SaveAsync(GameStateData data) {
             string serializedData = _serializer.Serialize(data);
-            await _storage.SaveAsync(key, serializedData);
+            await _storage.SaveAsync(serializedData);
         }
 
-        public async Task<T> LoadAsync<T>(string key) {
-            string serializedData = await _storage.LoadAsync(key);
-            return _serializer.Deserialize<T>(serializedData);
+        public async Task<GameStateData> LoadAsync(string key) {
+            var serializedData = await _storage.LoadAsync(key);
+            return _serializer.Deserialize<GameStateData>(serializedData);
         }
     }
 }
