bce3f2655600f03ee377c74cd05fd8d56de39a7d
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-07 22:29:32 +0300

    exposed a field for referencing the prefab bank

--- Assets/Code/GameCore/GameManager.cs
+++ Assets/Code/GameCore/GameManager.cs
@@ -1,9 +1,12 @@
-﻿using Code.Services.DataManagement.Serializers;
+﻿using Code.ScriptableObjectData;
+using Code.Services.DataManagement.Serializers;
 using Code.Services.DataManagement.Storage;
 using UnityEngine;
 
 namespace Code.GameCore {
     public class GameManager : MonoBehaviour {
+
+        [SerializeField] private PrefabBank _prefabBank;
 
         private GameStateManager _gameStateManager;
 
