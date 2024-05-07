27de5ed7ea77b8187e4acaab53b9778f0d4311dd
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-07 12:23:25 +0300

    Generic Save and Load Async methods

--- Assets/Code/Services/DataManagement/SaveManager.cs
+++ Assets/Code/Services/DataManagement/SaveManager.cs
@@ -1,12 +1,22 @@
-using System;
 using System.Threading.Tasks;
 
 public class SaveManager {
+
     private IStorage _storage;
     private ISerializer _serializer;
 
     public SaveManager(IStorage storage, ISerializer serialization) {
         _storage = storage;
         _serializer = serialization;
+    }
+
+    public async Task SaveAsync<T>(string key, T data) {
+        string serializedData = _serializer.Serialize(data);
+        await _storage.SaveAsync(key, serializedData);
+    }
+
+    public async Task<T> LoadAsync<T>(string key) {
+        string serializedData = await _storage.LoadAsync(key);
+        return _serializer.Deserialize<T>(serializedData);
     }
 }
