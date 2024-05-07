d1abcb3e6b790d6e4484bb5eaaf1ad247daba2de
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-07 19:00:58 +0300

    Created GameObjectFactory for the object creation

---
+++ Assets/Code/GameCore/Factories/GameObjectFactory.cs
@@ -0,0 +1,49 @@
+using Code.DataClasses;
+using Code.Services.DataManagement.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class GameObjectFactory {
+
+    private static Dictionary<string, Type> _instanceCreationMethods;
+    private ISerializer _serializer;
+
+    public GameObjectFactory(ISerializer serialization) {
+
+        _serializer = serialization;
+        _instanceCreationMethods = new Dictionary<string, Type>();
+        CreateDefaults();
+    }
+
+    private void CreateDefaults() {
+
+        Register("playerData", typeof(PlayerData));
+        //Register("doorData", typeof(DoorData));
+    }
+
+    public static void Register(string key, Type type) {
+        _instanceCreationMethods[key] = type;
+    }
+
+    public object Create(string key, string data) {
+
+        if (_instanceCreationMethods.ContainsKey(key)) {
+
+            Type objectType = _instanceCreationMethods[key];
+
+            MethodInfo deserializeMethod = _serializer.GetType().GetMethod("Deserialize").MakeGenericMethod(objectType);
+            object deserializedData = deserializeMethod.Invoke(_serializer, new object[] { data });
+
+            return deserializedData;
+        } else {
+
+            throw new KeyNotFoundException($"The key '{key}' does not exist in the registered instance creation methods.");
+        }
+
+    }
+
+    public IEnumerable<string> GetRegisteredKeys() {
+        return _instanceCreationMethods.Keys;
+    }
+}
