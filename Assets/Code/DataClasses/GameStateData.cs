97d24ca499290eca6d1e21094a9b234990c297ce
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-07 18:28:47 +0300

    class for holding the game state

---
+++ Assets/Code/DataClasses/GameStateData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code.DataClasses {
+    [Serializable]
+    public class GameStateData {
+        public List<ObjectRecord> objects;
+    }
+}
