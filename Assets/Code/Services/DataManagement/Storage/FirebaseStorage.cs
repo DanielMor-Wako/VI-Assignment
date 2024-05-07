14d70c9f58cde25eb313f49d0e53450fdca71be1
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-07 14:18:55 +0300

    Created Firebase storage, with Todos for future implementation

---
+++ Assets/Code/Services/DataManagement/Storage/FirebaseStorage.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Code.Services.DataManagement.Storage {
+    public class FirebaseStorage : IStorage {
+
+        public Task<string> LoadAsync(string key) {
+            // Todo: Load from storage
+            return null;
+        }
+
+        public Task SaveAsync(string key, string data) {
+            // Todo: Save to storage
+            return Task.CompletedTask;
+        }
+    }
+}
