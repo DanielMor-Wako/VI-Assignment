d66aa1e6318728bf4bd896cf5c1afad5b1b7a0b8
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-07 19:03:05 +0300

    Refactored IStorage to not include key

--- Assets/Code/Services/DataManagement/Storage/IStorage.cs
+++ Assets/Code/Services/DataManagement/Storage/IStorage.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 namespace Code.Services.DataManagement.Storage {
 
     public interface IStorage {
-        Task SaveAsync(string key, string data);
+        Task SaveAsync(string data);
         Task<string> LoadAsync(string key);
     }
 }
