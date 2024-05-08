fa21c465a327e73c86b27a0e70f80acef6ac7bac
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-08 12:12:17 +0300

    GameManager can located the PrefabBank by serialized field or by loading from the resources folder

--- Assets/Code/GameCore/GameManager.cs
+++ Assets/Code/GameCore/GameManager.cs
@@ -4,32 +4,45 @@ using Code.Services.DataManagement.Storage;
 using UnityEngine;
 
 namespace Code.GameCore {
+
     public class GameManager : MonoBehaviour {
 
         [SerializeField] private PrefabBank _prefabBank;
 
         private GameStateManager _gameStateManager;
 
-        private void Start() 
-        {
+
+        [ContextMenu("Load Game State")]
+        public void LoadGameState() {
+
+            _gameStateManager.LoadGameState();
+        }
+
+        [ContextMenu("Save Game State")]
+        public void SaveGameState() {
+
+            _gameStateManager.SaveGameState();
+        }
+
+        private void Start() {
+
             var serializer = new JsonSerializer();
             var storage = new FirebaseStorage();
+
+            GetPrefabBank();
 
             _gameStateManager = new GameStateManager(storage, serializer);
 
             LoadGameState();
         }
 
-        [ContextMenu("Load Game State")]
-        public void LoadGameState()
-        {
-            _gameStateManager.LoadGameState();
-        }
+        private void GetPrefabBank() {
 
-        [ContextMenu("Save Game State")]
-        public void SaveGameState()
-        {
-            _gameStateManager.SaveGameState();
+            _prefabBank ??= Resources.Load<PrefabBank>("PrefabBank/PrefabBank");
+
+            if (_prefabBank == null) {
+                Debug.LogError("PrefabBank not found in Resources folder. Please check that the PrefabBank is located at Assets/Resources/PrefabBank/PrefabBank.asset");
+            }
         }
     }
 }
