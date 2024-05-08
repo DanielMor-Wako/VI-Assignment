ab26376bc0b363e1594d66bc0bb7935617ee6d4b
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-08 20:14:27 +0300

    working system with testuser

--- Assets/Code/Services/DataManagement/Storage/FirebaseStorage.cs
+++ Assets/Code/Services/DataManagement/Storage/FirebaseStorage.cs
@@ -1,23 +1,50 @@
-﻿using Code.DataClasses;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Firebase.Database;
 
 namespace Code.Services.DataManagement.Storage {
     public class FirebaseStorage : IStorage {
 
+        private DatabaseReference _dbReference;
+
+        public FirebaseStorage() {
+            _dbReference = FirebaseDatabase.GetInstance("vi-assignment-default-rtdb.firebaseio.com").RootReference;
+        }
+
         public async Task<string> LoadAsync(string key) {
 
+            var result = string.Empty;
+
+            await _dbReference.Child("users").Child("testuser").GetValueAsync().ContinueWith(task => {
+                if (task.IsFaulted) {
+                    return;
+                }
+                if (task.IsCompleted) {
+                    DataSnapshot snapShot = task.Result;
+                    result = snapShot.GetRawJsonValue();
+                }
+            });
+
+            return result;
+
+            // Load from storage mock
             string playerData = "{\"displayName\":\"Shalom\", \"level\":10}";
             string tempString = "{\"objects\":[{\"key\":\"playerData\", \"value\":\"" + playerData.Replace("\"", "\\\"") + "\"}]}";
-
             return tempString;
-            
-            // Todo: Load from storage
         }
 
         public async Task SaveAsync(string data) {
 
-            
-            // Todo: Save to storage
+            // Save to storage
+            UnityEngine.Debug.Log("Started saving");
+            await _dbReference.Child("users").Child("testuser").SetRawJsonValueAsync(data).ContinueWith(task => {
+                if (task.IsFaulted) {
+                    return;
+                }
+                if (task.IsCompleted) {
+                    return;
+                }
+            });
+            UnityEngine.Debug.Log("Saving complete");
         }
 
     }
