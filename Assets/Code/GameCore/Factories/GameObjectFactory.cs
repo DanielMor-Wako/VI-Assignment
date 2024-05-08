a58372328b704336ca2e4ef6f259e3456e69b7f3
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-08 23:54:38 +0300

    namespace and small refactor

--- Assets/Code/GameCore/Factories/GameObjectFactory.cs
+++ Assets/Code/GameCore/Factories/GameObjectFactory.cs
@@ -4,46 +4,49 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 
-public class GameObjectFactory {
+namespace Code.GameCore.Factories {
 
-    private static Dictionary<string, Type> _instanceCreationMethods;
-    private ISerializer _serializer;
+    public class GameObjectFactory {
 
-    public GameObjectFactory(ISerializer serialization) {
+        private static Dictionary<string, Type> _instanceCreationMethods;
+        private ISerializer _serializer;
 
-        _serializer = serialization;
-        _instanceCreationMethods = new Dictionary<string, Type>();
-        CreateDefaults();
-    }
+        public GameObjectFactory(ISerializer serialization) {
 
-    private void CreateDefaults() {
-
-        Register("playerData", typeof(PlayerData));
-        //Register("doorData", typeof(DoorData));
-    }
-
-    public static void Register(string key, Type type) {
-        _instanceCreationMethods[key] = type;
-    }
-
-    public object Create(string key, string data) {
-
-        if (_instanceCreationMethods.ContainsKey(key)) {
-
-            Type objectType = _instanceCreationMethods[key];
-
-            MethodInfo deserializeMethod = _serializer.GetType().GetMethod("Deserialize").MakeGenericMethod(objectType);
-            object deserializedData = deserializeMethod.Invoke(_serializer, new object[] { data });
-
-            return deserializedData;
-        } else {
-
-            throw new KeyNotFoundException($"The key '{key}' does not exist in the registered instance creation methods.");
+            _serializer = serialization;
+            _instanceCreationMethods = new Dictionary<string, Type>();
+            CreateDefaults();
         }
 
-    }
+        private void CreateDefaults() {
 
-    public IEnumerable<string> GetRegisteredKeys() {
-        return _instanceCreationMethods.Keys;
+            Register("playerData", typeof(PlayerData));
+            //Register("doorData", typeof(DoorData));
+        }
+
+        public static void Register(string key, Type type) {
+            _instanceCreationMethods[key] = type;
+        }
+
+        public object Create(string key, string data) {
+
+            if (_instanceCreationMethods.ContainsKey(key)) {
+
+                Type objectType = _instanceCreationMethods[key];
+
+                MethodInfo deserializeMethod = _serializer.GetType().GetMethod("Deserialize").MakeGenericMethod(objectType);
+                object deserializedData = deserializeMethod.Invoke(_serializer, new object[] { data });
+
+                return deserializedData;
+            } else {
+
+                throw new KeyNotFoundException($"The key '{key}' does not exist in the registered instance creation methods.");
+            }
+
+        }
+
+        public IEnumerable<string> GetRegisteredKeys() {
+            return _instanceCreationMethods.Keys;
+        }
     }
 }
